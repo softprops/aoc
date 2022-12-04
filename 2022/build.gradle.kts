01ce4b60bb32e58554dc11f3d2902ec59695fb79
softprops <d.tangren@gmail.com>
2022-12-03 23:37:56 -0500

    remove gradle init added deps

--- 2022/build.gradle.kts
+++ 2022/build.gradle.kts
@@ -19,12 +19,6 @@ repositories {
 dependencies {
     // Use JUnit Jupiter for testing.
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:31.0.1-jre")
 }
 
 tasks.named<Test>("test") {
