cf4bf34c3ceab9edb4a71754f5cf93b94d811eae
softprops <d.tangren@gmail.com>
2022-12-05 18:43:44 -0500

    day one solution

--- 2022/src/test/java/aoc/Day1Test.java
+++ 2022/src/test/java/aoc/Day1Test.java
@@ -7,6 +7,7 @@ import static java.lang.Integer.parseInt;
 import static java.util.stream.Stream.concat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -46,33 +47,67 @@ class Day1Test {
             """
                 .replace("\n$", "")
                 .lines(),
-            24_000),
+            24_000,
+            true),
         Arguments.of(
             new String(Day1Test.class.getResourceAsStream("/input1.txt").readAllBytes()).lines(),
-            71_780));
+            71_780,
+            true),
+        Arguments.of(
+            """
+              1000
+              2000
+              3000
+
+              4000
+
+              5000
+              6000
+
+              7000
+              8000
+              9000
+
+              10000
+              """
+                .replace("\n$", "")
+                .lines(),
+            45_000,
+            false),
+        Arguments.of(
+            new String(Day1Test.class.getResourceAsStream("/input1.txt").readAllBytes()).lines(),
+            212_489,
+            false));
   }
 
-  int solve(Stream<String> lines) {
-    return lines
-        .reduce(
-            List.of(new Elf()),
-            (elves, line) ->
-                switch (line) {
-                  case "" -> concat(Stream.of(new Elf()), elves.stream()).toList();
-                  case String s -> concat(
-                          Stream.of(elves.get(0).add(parseInt(s))), elves.stream().skip(1))
-                      .toList();
-                },
-            (prev, next) -> concat(prev.stream(), next.stream()).toList())
-        .stream()
-        .mapToInt(Elf::calories)
-        .max()
-        .orElse(0);
+  int solve(Stream<String> lines, boolean partOne) {
+    var totals =
+        lines
+            .reduce(
+                List.of(new Elf()),
+                (elves, line) ->
+                    switch (line) {
+                      case "" -> concat(Stream.of(new Elf()), elves.stream()).toList();
+                      case String s -> concat(
+                              Stream.of(elves.get(0).add(parseInt(s))), elves.stream().skip(1))
+                          .toList();
+                    },
+                (prev, next) -> concat(prev.stream(), next.stream()).toList())
+            .stream()
+            .mapToInt(Elf::calories);
+
+    return partOne
+        ? totals.max().orElse(0)
+        : totals
+            .mapToObj(Integer::valueOf)
+            .sorted(Comparator.<Integer>naturalOrder().reversed())
+            .limit(3)
+            .reduce(0, Integer::sum);
   }
 
   @ParameterizedTest
   @MethodSource("args")
-  void test(Stream<String> lines, int expected) throws Exception {
-    assertEquals(expected, solve(lines));
+  void test(Stream<String> lines, int expected, boolean partOne) throws Exception {
+    assertEquals(expected, solve(lines, partOne));
   }
 }
