583ce51b7d2461587d76c6088c1987a2f2dd4f22
softprops <d.tangren@gmail.com>
2022-12-05 09:06:23 -0500

    solve day 2 add day 5

--- 2022/src/test/java/aoc/Day2Test.java
+++ 2022/src/test/java/aoc/Day2Test.java
@@ -21,6 +21,31 @@ class Day2Test {
     Outcome(int score) {
       this.score = score;
     }
+
+    static Outcome fromString(String str) {
+      return switch (str) {
+        case "X" -> Outcome.LOST;
+        case "Y" -> Outcome.DRAW;
+        case "Z" -> Outcome.WIN;
+        default -> throw new IllegalArgumentException();
+      };
+    }
+
+    Move strategicMove(Move other) {
+      return switch (this) {
+        case LOST -> switch (other) {
+          case ROCK -> Move.SCISSORS;
+          case PAPER -> Move.ROCK;
+          case SCISSORS -> Move.PAPER;
+        };
+        case DRAW -> other;
+        case WIN -> switch (other) {
+          case ROCK -> Move.PAPER;
+          case PAPER -> Move.SCISSORS;
+          case SCISSORS -> Move.ROCK;
+        };
+      };
+    }
   }
 
   enum Move {
@@ -64,19 +89,38 @@ class Day2Test {
             """
                 .replace("\n$", "")
                 .lines(),
-            15),
+            15,
+            true),
         Arguments.of(
             new String(Day1Test.class.getResourceAsStream("/input2.txt").readAllBytes()).lines(),
-            12_772));
+            12_772,
+            true),
+        Arguments.of(
+            """
+            A Y
+            B X
+            C Z
+            """
+                .replace("\n$", "")
+                .lines(),
+            12,
+            false),
+        Arguments.of(
+            new String(Day1Test.class.getResourceAsStream("/input2.txt").readAllBytes()).lines(),
+            11_618,
+            false));
   }
 
-  int solve(Stream<String> lines) {
+  int solve(Stream<String> lines, boolean partOne) {
     return lines.reduce(
         0,
         (total, line) -> {
           var round = line.split(" ");
           var theirMove = Move.fromString(round[0]);
-          var yourMove = Move.fromString(round[1]);
+          var yourMove =
+              partOne
+                  ? Move.fromString(round[1])
+                  : Outcome.fromString(round[1]).strategicMove(theirMove);
           return total + yourMove.outcome(theirMove).score + yourMove.score;
         },
         Integer::sum);
@@ -84,8 +128,8 @@ class Day2Test {
 
   @ParameterizedTest
   @MethodSource("args")
-  void test(Stream<String> lines, int expected) throws Exception {
-    int actual = solve(lines);
+  void test(Stream<String> lines, int expected, boolean partOne) throws Exception {
+    int actual = solve(lines, partOne);
     assertEquals(expected, actual);
   }
 }
