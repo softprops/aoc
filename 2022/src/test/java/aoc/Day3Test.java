acfa835d0005e4c23713478cd2934cbe262e454a
softprops <d.tangren@gmail.com>
2022-12-07 23:23:14 -0500

    solve day 3 part 2

--- 2022/src/test/java/aoc/Day3Test.java
+++ 2022/src/test/java/aoc/Day3Test.java
@@ -3,7 +3,7 @@
  */
 package aoc;
 
-import static java.util.function.Function.identity;
+import static java.lang.Math.min;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 import static java.util.stream.IntStream.rangeClosed;
@@ -21,35 +21,34 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 /** https://adventofcode.com/2022/day/3 */
 class Day3Test {
-  List<String> items =
-      IntStream.concat(rangeClosed('a', 'z'), rangeClosed('A', 'Z'))
-          .mapToObj(c -> String.valueOf((char) c))
-          .collect(toList());
-  Map<String, Integer> priorities =
-      rangeClosed(1, 52)
-          .mapToObj(Integer::valueOf)
-          .collect(toMap(p -> items.get(p - 1), identity()));
+  static int[] items = IntStream.concat(rangeClosed('a', 'z'), rangeClosed('A', 'Z')).toArray();
+  static Map<Integer, Item> catalog =
+      rangeClosed(1, 52).boxed().collect(toMap(p -> items[p - 1], Item::new));
 
-  record Rucksack(List<String> compartmentA, List<String> compartmentB) {
+  record Item(int priority) {}
+
+  record Rucksack(List<Item> compartmentA, List<Item> compartmentB) {
 
     public static Rucksack fromString(String str) {
       return new Rucksack(
-          str.substring(0, str.length() / 2)
-              .chars()
-              .mapToObj(c -> String.valueOf((char) c))
-              .collect(toList()),
+          str.substring(0, str.length() / 2).chars().boxed().map(catalog::get).collect(toList()),
           str.substring(str.length() / 2, str.length())
               .chars()
-              .mapToObj(c -> String.valueOf((char) c))
+              .boxed()
+              .map(catalog::get)
               .collect(toList()));
     }
 
-    public Optional<String> duplicate() {
-      var intersect = new HashSet<String>();
+    public List<Item> items() {
+      return Stream.concat(compartmentA.stream(), compartmentB.stream()).collect(toList());
+    }
+
+    public Optional<Integer> duplicatePriority() {
+      var intersect = new HashSet<Item>();
       intersect.addAll(compartmentA);
       intersect.retainAll(compartmentB);
       // System.out.println("intersect " + intersect);
-      return intersect.stream().findFirst();
+      return intersect.stream().findFirst().map(Item::priority);
     }
   }
 
@@ -66,23 +65,67 @@ class Day3Test {
             """
                 .replace("\n$", "")
                 .lines(),
-            157),
+            157,
+            true),
         Arguments.of(
             new String(Day1Test.class.getResourceAsStream("/input3.txt").readAllBytes()).lines(),
-            8_039));
+            8_039,
+            true),
+        Arguments.of(
+            """
+              vJrwpWtwJgWrhcsFMMfFFhFp
+              jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+              PmmdzqPrVvPwwTWBwg
+              wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+              ttgJtRGJQctTZtZT
+              CrZsJsPPZsGzwwsLwLmpwMDw
+              """
+                .replace("\n$", "")
+                .lines(),
+            70,
+            false),
+        Arguments.of(
+            new String(Day1Test.class.getResourceAsStream("/input3.txt").readAllBytes()).lines(),
+            2_510,
+            false));
   }
 
-  int solve(Stream<String> lines) {
-    return lines.reduce(
-        0,
-        (total, line) ->
-            total + Rucksack.fromString(line).duplicate().map(priorities::get).orElse(0),
-        Integer::sum);
+  <T> Stream<List<T>> grouped(Stream<T> stream, int size) {
+    var xs = stream.collect(toList());
+    return IntStream.range(0, (xs.size() + size - 1) / size)
+        .mapToObj(i -> xs.subList(i * size, min(size * (i + 1), xs.size())));
+  }
+
+  int solve(Stream<String> lines, boolean partOne) {
+    if (partOne) {
+      return lines.reduce(
+          0,
+          (total, line) -> total + Rucksack.fromString(line).duplicatePriority().orElse(0),
+          Integer::sum);
+    }
+    return grouped(lines, 3)
+        .reduce(
+            0,
+            (total, group) -> {
+              var cadidates =
+                  group.stream()
+                      .skip(1)
+                      .map(Rucksack::fromString)
+                      .reduce(
+                          new HashSet<Item>(Rucksack.fromString(group.get(0)).items()),
+                          (same, rucksack) -> {
+                            same.retainAll(rucksack.items());
+                            return same;
+                          },
+                          (prev, next) -> next);
+              return total + cadidates.stream().findFirst().map(Item::priority).orElse(0);
+            },
+            Integer::sum);
   }
 
   @ParameterizedTest
   @MethodSource("args")
-  void test(Stream<String> lines, int expected) throws Exception {
-    assertEquals(expected, solve(lines));
+  void test(Stream<String> lines, int expected, boolean partOne) throws Exception {
+    assertEquals(expected, solve(lines, partOne));
   }
 }
