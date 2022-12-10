fef5307bdf8f7c5cd90c41930f1b8044c219bbcc
softprops <d.tangren@gmail.com>
2022-12-10 11:17:57 -0500

    better test ux

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,21 @@
 plugins { 
   java
+  id("com.adarshr.test-logger") version "3.2.0"
 }
 
 subprojects {
  
   apply(plugin = "java")
+  apply(plugin = "com.adarshr.test-logger")
 
   java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
+  }
+
+  testlogger {
+    theme = com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA
   }
 
   tasks.withType<JavaCompile>().configureEach {
